242700c16a97a6896dfd3f448e2a0e008e1bdf4c
王晓清 <805380422@qq.com>
2016-08-17 19:32:03 +0800

    baseactivity 以及 baseapplication

---
+++ wxqutilslibrary/src/main/java/com/example/wxq/wxqutilslibrary/activity/BaseActivity.java
@@ -0,0 +1,90 @@
+package com.example.wxq.wxqutilslibrary.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.example.wxq.wxqutilslibrary.myutils.log.LogUtils;
+
+/**
+ * Created by Administrator on 2016/8/17 0017.
+ * 主要功能为：
+ * 分功能为：
+ * 分功能为：
+ */
+public abstract class BaseActivity extends FragmentActivity {
+public String className="";
+
+
+    @Override
+        public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+            super.onCreate(savedInstanceState, persistentState);
+           className=this.getClass().getName();
+           beforeSetView();
+           setContentView(setActivityView());
+
+        }
+
+    private void beforeSetView() {
+    }
+
+    //设置activity的view
+    public  abstract int  setActivityView();
+
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    //ctrl alt k
+public void showLog(String content){
+    LogUtils.i(className,content);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ wxqutilslibrary/src/main/java/com/example/wxq/wxqutilslibrary/application/BaseApplication.java
@@ -0,0 +1,130 @@
+package com.example.wxq.wxqutilslibrary.application;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Build;
+
+import java.util.Stack;
+
+import Tools.LogUtils;
+
+import static Tools.LogUtils.*;
+
+public abstract class BaseApplication extends Application {
+    /*
+    * 初始化TAG
+    * */
+    private static String TAG = BaseApplication.class.getName();
+
+    /*Activity堆*/
+    private Stack<Activity> activityStack = new Stack<Activity>();
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        d(TAG, TAG + "---onCreate()");
+
+
+
+        printAppParameter();
+        initOthers();
+    }
+
+
+    public abstract void initOthers();
+
+    /*打印出一些app的参数*/
+    public abstract void printAppParameter();
+
+    public void addActivity(final Activity curAT) {
+        if (null == activityStack) {
+            activityStack = new Stack<Activity>();
+        }
+        activityStack.add(curAT);
+    }
+
+    /**
+     * 获取栈顶Activity（堆栈中最后一个压入的）
+     */
+    public Activity getTopActivity() {
+        if (activityStack != null) {
+            Activity activity = activityStack.lastElement();
+            return activity;
+        }
+        return null;
+    }
+
+    /**
+     * 结束栈顶Activity（堆栈中最后一个压入的）
+     */
+    public void killTopActivity() {
+        if (activityStack != null) {
+            Activity activity = activityStack.lastElement();
+            killActivity(activity);
+        }
+    }
+
+
+    public void removeActivity(final Activity curAT) {
+        if (null == activityStack) {
+            activityStack = new Stack<Activity>();
+        }
+        activityStack.remove(curAT);
+    }
+
+    /**
+     * 结束指定类名的Activity
+     */
+    public void killActivity(Class<?> cls) {
+        if (cls != null && activityStack != null) {
+            for (int i = activityStack.size() - 1; i >= 0; i--) {
+                if (activityStack.get(i).getClass().equals(cls)) {
+                    killActivity(activityStack.get(i));
+                }
+            }
+        }
+    }
+
+    /**
+     * 结束指定的Activity
+     */
+    public void killActivity(Activity activity) {
+        if (activity != null && activityStack != null) {
+            if (activityStack.contains(activity)) {
+                activityStack.remove(activity);
+                activity.finish();
+                activity = null;
+            }
+        }
+    }
+
+    //获取最后一个Activity
+    public Activity currentActivity() {
+        Activity activity = activityStack.lastElement();
+        return activity;
+    }
+
+    //返回寨内Activity的总数
+    public int howManyActivities() {
+        return activityStack.size();
+    }
+
+    //关闭所有Activity
+    public void finishAllActivities() {
+        for (int i = 0, size = activityStack.size(); i < size; i++) {
+            if (null != activityStack.get(i)) {
+                activityStack.get(i).finish();
+            }
+        }
+        activityStack.clear();
+    }
+
+    public void exit() {
+
+        finishAllActivities();
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(0);
+    }
+
+}
