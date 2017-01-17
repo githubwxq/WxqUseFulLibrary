970ab3a9b92c2bd0179a45558e9b665d6701835e
王晓清 <805380422@qq.com>
2017-01-17 18:09:40 +0800

    base

--- wxqutilslibrary/src/main/java/com/example/wxq/wxqutilslibrary/application/BaseApplication.java
+++ wxqutilslibrary/src/main/java/com/example/wxq/wxqutilslibrary/application/BaseApplication.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 
 import com.example.wxq.wxqutilslibrary.myutils.imageloader.LoadingImgUtil;
@@ -13,9 +14,14 @@ import com.umeng.analytics.MobclickAgent;
 
 import org.xutils.x;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import specialtools.ActivityManager;
 
@@ -29,6 +35,7 @@ public abstract class BaseApplication extends Application implements Thread.Unca
     }
 
     private RefWatcher refWatcher;
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -53,7 +60,7 @@ public abstract class BaseApplication extends Application implements Thread.Unca
             return;
         }
         refWatcher = LeakCanary.install(this);
-         LeakCanary.install(this);
+        LeakCanary.install(this);
         initResourceAndother();
         this.registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
             @Override
@@ -107,6 +114,8 @@ public abstract class BaseApplication extends Application implements Thread.Unca
     @Override
     public void uncaughtException(Thread thread, Throwable ex) {
         Log.e("wxq", outputError(ex));
+        String time = getErrorTime();
+        writeAndUpload(mContext, time + outputError(ex).substring(16, 35), outputError(ex));//错误写入到sd卡中
         dealWithException(ex);// 给上层处理异常  可以重启服务发警告等等
         MobclickAgent.onKillProcess(this);
         ActivityManager.getInstance().AppExit(mContext);
@@ -145,6 +154,59 @@ public abstract class BaseApplication extends Application implements Thread.Unca
         } catch (Exception e) {
         }
     }
+   //存放错误到本地wxqexception文件夹 可以上传到服务器
+    public void writeAndUpload(Context context, String key, String value) {
+        File cacheDir = getCacheFile(context);
+        FileWriter writer = null;
+        File cacheFile = new File(cacheDir, key + ".txt");
+        if (!cacheFile.exists()) {
+            try {
+                cacheFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            writer = new FileWriter(cacheFile, true);
+            writer.write(value);// 写入缓存
+            writer.write("\n");
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public File getCacheFile(Context context) {
+        File cacheFile = null;
+        String externalStorageState = Environment.getExternalStorageState();
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            cacheFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "WXQEXCEPTION");
+            if (!cacheFile.exists()) {
+                cacheFile.mkdir();
+            }
+        } else {
+            cacheFile = context.getCacheDir();
+        }
+        return cacheFile;
+    }
+
+    public String getErrorTime() {
+        SimpleDateFormat formatter = new SimpleDateFormat("MM月dd日_HH:mm:ss");
+        Date curDate = new Date(System.currentTimeMillis());//获取当前时间
+        return formatter.format(curDate);
+    }
+
+
+
 
 
 }
